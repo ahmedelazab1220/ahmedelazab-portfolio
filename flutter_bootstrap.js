6f737b26237ee6b7d7020760b410089a5691dd1c
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2024-12-08 07:12:25 +0200

    Add Some Modification.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4222036467"
+    serviceWorkerVersion: "836509719"
   }
 });
